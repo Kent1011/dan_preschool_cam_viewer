9f9bd6736493682771dbec79e706e980ec43356a
Kent Chien <kent@myre.life>
2021-11-19 16:38:56 +0800

    refactor: remove useless import

--- lib/core/api_service/api_service_provider.dart
+++ lib/core/api_service/api_service_provider.dart
@@ -1,5 +1,4 @@
 import 'package:dan_preschool_cam_viewer/core/api_service/api_service.dart';
-import 'package:dan_preschool_cam_viewer/core/application/app_providers.dart';
 import 'package:dan_preschool_cam_viewer/core/application/arguments.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
